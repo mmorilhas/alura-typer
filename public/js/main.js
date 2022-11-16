3db397630960031dbade321523914b3608e7f33e
Michelle Morilhas <mmorilhas@magnasistemas.com.br>
2022-11-16 14:47:53 -0300

    funcionalidade de reiniciar o jogo

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,68 @@
-let frase = $(".frase").text();
-let numPalavras = frase.split(" ").length;
-let tamanhoFrase = $("#tamanho-frase");
+$(document).ready(function(){
+  atualizaFrase();
+  contaInput();
+  calculaTempo();
+  $("#btn-reiniciar").click(reiniciarJogo);
+})
 
-tamanhoFrase.text(numPalavras);
-
-
-console.log(frase);
-console.log(numPalavras);
+function atualizaFrase(){
+  let frase = $(".frase").text();
+  let numPalavras = frase.split(" ").length;
+  let tamanhoFrase = $("#tamanho-frase");
+  
+  tamanhoFrase.text(numPalavras);
+  
+}
 
 
 // text-area
-
 let campoTexto = $(".campo-texto");
-campoTexto.on("input", function(){
+
+function contaInput(){
+  campoTexto.on("input", function(){
  
- let conteudo = campoTexto.val();
+    let conteudo = campoTexto.val();
+   
+    let qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+   
+    let qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+   
+   })
+}
 
- let qtdCaracteres = conteudo.length;
- $("#contador-caracteres").text(qtdCaracteres);
-
- let qtdPalavras = conteudo.split(/\S+/).length - 1;
- $("#contador-palavras").text(qtdPalavras);
-
-})
 
 // contador tempo
+let tempoInicial = $("#contador-tempo").text();
 
-let contadorTempo = $("#contador-tempo").text();
-campoTexto.one("focus", function(){
-  let cronometroID = setInterval(function(){
-    contadorTempo--;
-    $("#contador-tempo").text(contadorTempo);
-    if(contadorTempo < 1){
-      campoTexto.attr("disabled", true);
-      clearInterval(cronometroID);
-    }
-  }, 1000);
+function calculaTempo(){
+  let contadorTempo = $("#contador-tempo").text();
+  campoTexto.one("focus", function(){
+    let cronometroID = setInterval(function(){
+      contadorTempo--;
+      $("#contador-tempo").text(contadorTempo);
+      if(contadorTempo < 1){
+        campoTexto.attr("disabled", true);
+        clearInterval(cronometroID);
+      }
+    }, 1000);
+    
+  });
+}
+
+
+
+// reiniicar jogo
+function reiniciarJogo(){
+
+  campoTexto.attr("disabled", false);
+  campoTexto.val("");
+  $("#contador-tempo").text(tempoInicial);
+  $("#contador-caracteres").text("0");
+  $("#contador-palavras").text("0");
+  calculaTempo(); 
   
-  
-});
+}
 
 
-// reinicar jogo
-$("#btn-reiniciar").click(function(){
-  console.log("cliquei")
-})
