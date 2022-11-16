2548884f2b39998bcbb11d4976cc0c11ccc4f8a4
Michelle Morilhas <mmorilhas@magnasistemas.com.br>
2022-11-16 12:11:08 -0300

    adicionando text area

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,25 @@ let frase = $(".frase").text();
 let numPalavras = frase.split(" ").length;
 let tamanhoFrase = $("#tamanho-frase");
 
-tamanhoFrase.text(numPalavras)
-
+tamanhoFrase.text(numPalavras);
 
 
 console.log(frase);
 console.log(numPalavras);
+
+
+// text-area
+
+let campoTexto = $(".campo-texto");
+campoTexto.on("input", function(){
+ 
+ let conteudo = campoTexto.val();
+
+ let qtdCaracteres = conteudo.length;
+ $("#contador-caracteres").text(qtdCaracteres);
+
+ let qtdPalavras = conteudo.split(/\S+/).length - 1;
+ $("#contador-palavras").text(qtdPalavras);
+
+})
+
